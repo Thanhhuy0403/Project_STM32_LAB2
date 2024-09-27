45aa487f689da9da339e74e7a4f9f837cedf1bec
Thanhuy0403 <huythanhphan2020fpt@gmail.com>
2024-09-27 18:08:02 +0700

    fix bug

--- Project_STM32_LAB2/Core/Src/exericise1.c
+++ Project_STM32_LAB2/Core/Src/exericise1.c
@@ -40,20 +40,6 @@ void display7SEG(int num){
 void initExercise1(){
 	HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
 	HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-}
-
-static bool status_led = true; // true -> led1 on, false -> led2 on (7SEG)
-void runExercise1(){
-	if(status_led){
-		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
-		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-		display7SEG(1);
-	}else{
-		HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-		HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
-		display7SEG(2);
-	}
-	status_led = !status_led;
 	HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
 	HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
 }
